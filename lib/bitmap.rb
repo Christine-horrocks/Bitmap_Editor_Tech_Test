7d4511d3a6d7c60c89f925acf10cd03a874af229
Christine <splogy2001@hotmail.com>
2018-01-16 19:41:31 +0000

    x y in proper order

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -5,11 +5,11 @@ class Bitmap
   def initialize(m, n, c = "O")
     @m = m
     @n = n
-    @bitmap = Array.new(m) { Array.new(n, c)}
+    @bitmap = Array.new(n) { Array.new(m, c)}
   end
 
   def clear
-    @bitmap = Array.new(m) { Array.new(n, "O")}
+    @bitmap = Array.new(n) { Array.new(m, "O")}
   end
 
   def show
@@ -19,6 +19,12 @@ class Bitmap
   def pixel_change(x, y, c)
     bmap = @bitmap
     bmap[y-1][x-1] = c
+  end
+
+  def vertical_change(x, y1, y2, c)
+    bmap = @bitmap
+    bmap[y1-1][x-1] = c
+    bmap[y2-1][x-1] = c
   end
 
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -3,36 +3,40 @@ require "bitmap"
 describe Bitmap do
 
   it 'insantiates @bitmap with input dimentions' do
-    bmap = Bitmap.new(2, 3)
+    bmap = Bitmap.new(3, 2)
     expect(bmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
   end
 
   it 'resets the @bitmap' do
-    bmap = Bitmap.new(2, 3, "B")
+    bmap = Bitmap.new(3, 2, "B")
     bmap.clear
     expect(bmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
   end
 
   it 'shows the @bitmap' do
-    bmap = Bitmap.new(2, 3)
+    bmap = Bitmap.new(3, 2)
     expect(bmap.show).to eq("OOO\nOOO\n")
   end
 
   it 'changes a pixel colour' do
-    bmap = Bitmap.new(2, 3)
+    bmap = Bitmap.new(3, 2)
     bmap.pixel_change(1, 2, "B")
     expect(bmap.bitmap).to eq([["O", "O", "O"], ["B", "O", "O"]])
   end
 
   it 'changes a pixel colour can be shown' do
-    bmap = Bitmap.new(2, 3)
+    bmap = Bitmap.new(3, 2)
     bmap.pixel_change(3, 2, "B")
     expect(bmap.show).to eq("OOO\nOOB\n")
   end
 
   it 'changes a vertical line of pixels colour' do
-    bmap = Bitmap.new(2, 3)
+    bmap = Bitmap.new(3, 2)
     bmap.vertical_change(1, 1, 2, "B")
     expect(bmap.bitmap).to eq([["B", "O", "O"], ["B", "O", "O"]])
   end
+
+
+
+
 end
