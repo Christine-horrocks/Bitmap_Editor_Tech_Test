3649be24b7123612e65303ed7bc939c54e2e41e2
Christine <splogy2001@hotmail.com>
2018-01-16 17:27:02 +0000

    clear method tested and added

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,8 +1,15 @@
 class Bitmap
-  attr_reader :bitmap
+  attr_accessor :bitmap
+  attr_reader :m, :n
 
-  def initialize()
-    @bitmap = []
-  end 
+  def initialize(m, n, c = "O")
+    @m = m
+    @n = n
+    @bitmap = Array.new(m) { Array.new(n, c)}
+  end
+
+  def clear
+    @bitmap = Array.new(m) { Array.new(n, "O")}
+  end
 
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -2,7 +2,14 @@ require "bitmap"
 
 describe Bitmap do
 
-  it 'insantiates with @bitmap' do
-    expect(subject.bitmap).to eq([])
+  it 'insantiates @bitmap with input dimentions' do
+    bmap = Bitmap.new(2, 3)
+    expect(bmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
+  end
+
+  it 'resets the @bitmap' do
+    bmap = Bitmap.new(2, 3, "B")
+    bmap.clear
+    expect(bmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
   end
 end
