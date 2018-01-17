2238b9189daf10f7da0129f71fdef8dec794913c
Christine <splogy2001@hotmail.com>
2018-01-17 12:55:54 +0000

    refactor and finish vertical change

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -13,18 +13,17 @@ class Bitmap
   end
 
   def show
-    bmap = @bitmap.map { |array| array.push("\n")}.join
+    @bitmap.map { |array| array.push("\n")}.join
   end
 
   def pixel_change(x, y, c)
-    bmap = @bitmap
-    bmap[y-1][x-1] = c
+    @bitmap[y-1][x-1] = c
   end
 
   def vertical_change(x, y1, y2, c)
-    bmap = @bitmap
-    bmap[y1-1][x-1] = c
-    bmap[y2-1][x-1] = c
+    (y1..y2).each do |e|
+      @bitmap[e-1][x-1] = c
+    end
   end
 
 end
