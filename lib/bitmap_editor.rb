44b67a0485c200bf429dfb595c4aa9897b5a341b
Christine <splogy2001@hotmail.com>
2018-01-18 19:23:29 +0000

    fix for coordinates with mulitple digets

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -10,7 +10,7 @@ class BitmapEditor
     # return puts "please provide correct file" if file.nil? || !File.exists?(file)
 
     File.open(file).each do |line|
-      line = line.chomp.scan /\w/
+      line = line.chomp.split
       case line[0].upcase
       when "I"
         @bitmap = Bitmap.new(line[1].to_i, line[2].to_i)
--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -10,60 +10,60 @@ describe BitmapEditor do
   it 'indicates if command letter not valid' do
     content = ["P"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    expect { subject.run("show.txt") }.to output("unrecognised command :(\n").to_stdout
+    expect { subject.run(@filename) }.to output("unrecognised command :(\n").to_stdout
   end
 
   it 'instantiates a bitmap with command I' do
     content = ["I 2 3"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    subject.run("show.txt")
+    subject.run(@filename)
     expect(subject.bitmap.bitmap).to eq([["O", "O"], ["O", "O"], ["O", "O"]])
   end
 
   it 'shows the bitmap with command S' do
     content = ["I 3 2", "S"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    expect { subject.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
+    expect { subject.run(@filename) }.to output("OOO\nOOO\n").to_stdout
   end
 
   it 'still works with command lowercase' do
     content = ["I 3 2", "s"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    expect { subject.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
+    expect { subject.run(@filename) }.to output("OOO\nOOO\n").to_stdout
   end
 
   it 'clears the bitmap with command C' do
     content = ["I 3 2", "C"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    subject.run("show.txt")
+    subject.run(@filename)
     expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
   end
 
   it 'colours a pixel with command L' do
     content = ["I 3 2", "L 2 2 B"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    subject.run("show.txt")
+    subject.run(@filename)
     expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "B", "O"]])
   end
 
   it 'colours a vertical with command V' do
     content = ["I 3 3", "V 2 1 3 B"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    subject.run("show.txt")
+    subject.run(@filename)
     expect(subject.bitmap.bitmap).to eq([["O", "B", "O"], ["O", "B", "O"], ["O", "B", "O"]])
   end
 
   it 'colours a horizontal with command H' do
     content = ["I 3 2", "H 1 2 2 B"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    subject.run("show.txt")
+    subject.run(@filename)
     expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["B", "B", "O"]])
   end
 
   it 'raises and error if there is no bitmap' do
     content = ["H 1 2 2 B"]
     allow(File).to receive(:open).with(@filename).and_return(content)
-    expect { subject.run("show.txt") }.to raise_error 'You need to create a bitmap first'
+    expect { subject.run(@filename) }.to raise_error 'You need to create a bitmap first'
   end
 
 end
