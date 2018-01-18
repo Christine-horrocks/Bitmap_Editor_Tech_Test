6b71499f2f28d87b6a81122475e2af9495d7ac0f
Christine <splogy2001@hotmail.com>
2018-01-18 17:55:18 +0000

    other tests passing

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -19,30 +19,30 @@ describe BitmapEditor do
     allow(File).to receive(:open).with(filename).and_return(content)
     expect { editor.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
   end
-  #
-  # it 'still works with command lowercase' do
-  #   editor = BitmapEditor.new
-  #   filename = "show.txt"
-  #   content = ["s"]
-  #   allow(File).to receive(:open).with(filename).and_return(content)
-  #   expect { editor.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
-  # end
-  #
-  # it 'raises an error if command letter not valid' do
-  #   editor = BitmapEditor.new
-  #   filename = "show.txt"
-  #   content = ["P"]
-  #   allow(File).to receive(:open).with(filename).and_return(content)
-  #   expect { editor.run("show.txt") }.to output("unrecognised command :(\n").to_stdout
-  # end
-  #
-  # it "clears the bitmap" do
-  #   editor = BitmapEditor.new
-  #   filename = "show.txt"
-  #   content = ["C"]
-  #   allow(File).to receive(:open).with(filename).and_return(content)
-  #   editor.run("show.txt")
-  #   expect(editor.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
-  # end
+
+  it 'still works with command lowercase' do
+    editor = BitmapEditor.new
+    filename = "show.txt"
+    content = ["I 3 2", "s"]
+    allow(File).to receive(:open).with(filename).and_return(content)
+    expect { editor.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
+  end
+
+  it 'raises an error if command letter not valid' do
+    editor = BitmapEditor.new
+    filename = "show.txt"
+    content = ["P"]
+    allow(File).to receive(:open).with(filename).and_return(content)
+    expect { editor.run("show.txt") }.to output("unrecognised command :(\n").to_stdout
+  end
+
+  it "clears the bitmap" do
+    editor = BitmapEditor.new
+    filename = "show.txt"
+    content = ["I 3 2", "C"]
+    allow(File).to receive(:open).with(filename).and_return(content)
+    editor.run("show.txt")
+    expect(editor.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
+  end
 
 end
