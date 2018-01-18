033e851f55b7ece54fadf50e160285c5552cd1e3
Christine <splogy2001@hotmail.com>
2018-01-18 19:07:39 +0000

    more refactoring of tests

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -3,71 +3,66 @@ require "bitmap"
 
 describe BitmapEditor do
 
+  before(:all) do
+    @filename = "show.txt"
+  end
+
   it 'indicates if command letter not valid' do
-    filename = "show.txt"
     content = ["P"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     expect { subject.run("show.txt") }.to output("unrecognised command :(\n").to_stdout
   end
 
   it 'instantiates a bitmap with command I' do
-    filename = "show.txt"
     content = ["I 2 3"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     subject.run("show.txt")
     expect(subject.bitmap.bitmap).to eq([["O", "O"], ["O", "O"], ["O", "O"]])
   end
 
   it 'shows the bitmap with command S' do
-    filename = "show.txt"
     content = ["I 3 2", "S"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     expect { subject.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
   end
 
   it 'still works with command lowercase' do
-    filename = "show.txt"
     content = ["I 3 2", "s"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     expect { subject.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
   end
 
   it 'clears the bitmap with command C' do
-    filename = "show.txt"
     content = ["I 3 2", "C"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     subject.run("show.txt")
     expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
   end
 
   it 'colours a pixel with command L' do
-    filename = "show.txt"
     content = ["I 3 2", "L 2 2 B"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     subject.run("show.txt")
     expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "B", "O"]])
   end
 
   it 'colours a vertical with command V' do
-    filename = "show.txt"
     content = ["I 3 3", "V 2 1 3 B"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     subject.run("show.txt")
     expect(subject.bitmap.bitmap).to eq([["O", "B", "O"], ["O", "B", "O"], ["O", "B", "O"]])
   end
 
   it 'colours a horizontal with command H' do
-    filename = "show.txt"
     content = ["I 3 2", "H 1 2 2 B"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     subject.run("show.txt")
     expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["B", "B", "O"]])
   end
 
   it 'raises and error if there is no bitmap' do
-    filename = "show.txt"
     content = ["H 1 2 2 B"]
-    allow(File).to receive(:open).with(filename).and_return(content)
+    allow(File).to receive(:open).with(@filename).and_return(content)
     expect { subject.run("show.txt") }.to raise_error 'You need to create a bitmap first'
   end
 
