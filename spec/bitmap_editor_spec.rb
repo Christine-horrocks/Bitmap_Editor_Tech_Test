d895863633d4fcacc1635d5e98a35c2a524aa8b3
Christine <splogy2001@hotmail.com>
2018-01-18 19:04:34 +0000

    refactor tests

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -4,80 +4,71 @@ require "bitmap"
 describe BitmapEditor do
 
   it 'indicates if command letter not valid' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["P"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    expect { editor.run("show.txt") }.to output("unrecognised command :(\n").to_stdout
+    expect { subject.run("show.txt") }.to output("unrecognised command :(\n").to_stdout
   end
 
   it 'instantiates a bitmap with command I' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["I 2 3"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    editor.run("show.txt")
-    expect(editor.bitmap.bitmap).to eq([["O", "O"], ["O", "O"], ["O", "O"]])
+    subject.run("show.txt")
+    expect(subject.bitmap.bitmap).to eq([["O", "O"], ["O", "O"], ["O", "O"]])
   end
 
   it 'shows the bitmap with command S' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["I 3 2", "S"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    expect { editor.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
+    expect { subject.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
   end
 
   it 'still works with command lowercase' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["I 3 2", "s"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    expect { editor.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
+    expect { subject.run("show.txt") }.to output("OOO\nOOO\n").to_stdout
   end
 
   it 'clears the bitmap with command C' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["I 3 2", "C"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    editor.run("show.txt")
-    expect(editor.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
+    subject.run("show.txt")
+    expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "O", "O"]])
   end
 
   it 'colours a pixel with command L' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["I 3 2", "L 2 2 B"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    editor.run("show.txt")
-    expect(editor.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "B", "O"]])
+    subject.run("show.txt")
+    expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["O", "B", "O"]])
   end
 
   it 'colours a vertical with command V' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["I 3 3", "V 2 1 3 B"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    editor.run("show.txt")
-    expect(editor.bitmap.bitmap).to eq([["O", "B", "O"], ["O", "B", "O"], ["O", "B", "O"]])
+    subject.run("show.txt")
+    expect(subject.bitmap.bitmap).to eq([["O", "B", "O"], ["O", "B", "O"], ["O", "B", "O"]])
   end
 
   it 'colours a horizontal with command H' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["I 3 2", "H 1 2 2 B"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    editor.run("show.txt")
-    expect(editor.bitmap.bitmap).to eq([["O", "O", "O"], ["B", "B", "O"]])
+    subject.run("show.txt")
+    expect(subject.bitmap.bitmap).to eq([["O", "O", "O"], ["B", "B", "O"]])
   end
 
   it 'raises and error if there is no bitmap' do
-    editor = BitmapEditor.new
     filename = "show.txt"
     content = ["H 1 2 2 B"]
     allow(File).to receive(:open).with(filename).and_return(content)
-    expect {editor.run("show.txt")}.to raise_error 'You need to create a bitmap first'
+    expect { subject.run("show.txt") }.to raise_error 'You need to create a bitmap first'
   end
 
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -56,30 +56,29 @@ describe Bitmap do
 
   context "pixel change" do
 
+    before(:context) do
+     @bmap = Bitmap.new(3, 2)
+    end
+
     it 'changes a pixel colour' do
-      bmap = Bitmap.new(3, 2)
-      bmap.pixel_change(1, 2, "B")
-      expect(bmap.bitmap).to eq([["O", "O", "O"], ["B", "O", "O"]])
+      @bmap.pixel_change(1, 2, "B")
+      expect(@bmap.bitmap).to eq([["O", "O", "O"], ["B", "O", "O"]])
     end
 
     it 'raises an error if x not integer' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.pixel_change("p", 2, "B")}.to raise_error 'Your x coordinate must be an integer'
+      expect {@bmap.pixel_change("p", 2, "B")}.to raise_error 'Your x coordinate must be an integer'
     end
 
     it 'raises an error if x out of range' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.pixel_change(4, 2, "B")}.to raise_error 'Your x coordinate is out of range'
+      expect {@bmap.pixel_change(4, 2, "B")}.to raise_error 'Your x coordinate is out of range'
     end
 
     it 'raises an error if y not integer' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.pixel_change(1, "p", "B")}.to raise_error 'Your y coordinate must be an integer'
+      expect {@bmap.pixel_change(1, "p", "B")}.to raise_error 'Your y coordinate must be an integer'
     end
 
     it 'raises an error if y out of range' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.pixel_change(3, 3, "B")}.to raise_error 'Your y coordinate is out of range'
+      expect {@bmap.pixel_change(3, 3, "B")}.to raise_error 'Your y coordinate is out of range'
     end
 
     it 'changes a pixel colour can be shown' do
@@ -98,92 +97,84 @@ describe Bitmap do
 
   context "vertical_change" do
 
+    before(:context) do
+     @bmap = Bitmap.new(3, 2)
+    end
+
     it 'changes a vertical line of pixels colour' do
-      bmap = Bitmap.new(3, 2)
-      bmap.vertical_change(1, 1, 2, "B")
-      expect(bmap.bitmap).to eq([["B", "O", "O"], ["B", "O", "O"]])
+      @bmap.vertical_change(1, 1, 2, "B")
+      expect(@bmap.bitmap).to eq([["B", "O", "O"], ["B", "O", "O"]])
     end
 
     it 'raises an error if x not integer' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.vertical_change("p", 1, 2, "B")}.to raise_error 'Your x coordinate must be an integer'
+      expect {@bmap.vertical_change("p", 1, 2, "B")}.to raise_error 'Your x coordinate must be an integer'
     end
 
     it 'raises an error if x out of range' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.vertical_change(4, 1, 2, "B")}.to raise_error 'Your x coordinate is out of range'
+      expect {@bmap.vertical_change(4, 1, 2, "B")}.to raise_error 'Your x coordinate is out of range'
     end
 
     it 'raises an error if y1 not integer' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.vertical_change(3, "p", 1, "B")}.to raise_error 'Your y1 coordinate must be an integer'
+      expect {@bmap.vertical_change(3, "p", 1, "B")}.to raise_error 'Your y1 coordinate must be an integer'
     end
 
     it 'raises an error if y1 out of range' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.vertical_change(3, 4, 1, "B")}.to raise_error 'Your y1 coordinate is out of range'
+      expect {@bmap.vertical_change(3, 4, 1, "B")}.to raise_error 'Your y1 coordinate is out of range'
     end
 
     it 'raises an error if y2 not integer' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.vertical_change(3, 1, "p", "B")}.to raise_error 'Your y2 coordinate must be an integer'
+      expect {@bmap.vertical_change(3, 1, "p", "B")}.to raise_error 'Your y2 coordinate must be an integer'
     end
 
     it 'raises an error if y2 out of range' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.vertical_change(3, 1, 4, "B")}.to raise_error 'Your y2 coordinate is out of range'
+      expect {@bmap.vertical_change(3, 1, 4, "B")}.to raise_error 'Your y2 coordinate is out of range'
     end
 
     it 'colour represented by capital letter' do
-      bmap = Bitmap.new(3, 2)
-      bmap.vertical_change(1, 1, 2, "b")
-      expect(bmap.bitmap).to eq([["B", "O", "O"], ["B", "O", "O"]])
+      @bmap.vertical_change(1, 1, 2, "b")
+      expect(@bmap.bitmap).to eq([["B", "O", "O"], ["B", "O", "O"]])
     end
 
   end
 
   context "horizontal change" do
 
+    before(:context) do
+     @bmap = Bitmap.new(3, 2)
+    end
+
     it 'changes a horizontal line of pixels colour' do
-      bmap = Bitmap.new(3, 2)
-      bmap.horizontal_change(1, 3, 1,"B")
-      expect(bmap.bitmap).to eq([["B", "B", "B"], ["O", "O", "O"]])
+      @bmap.horizontal_change(1, 3, 1,"B")
+      expect(@bmap.bitmap).to eq([["B", "B", "B"], ["O", "O", "O"]])
     end
 
     it 'raises an error if x1 not integer' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.horizontal_change("p", 2, 1, "B")}.to raise_error 'Your x1 coordinate must be an integer'
+      expect {@bmap.horizontal_change("p", 2, 1, "B")}.to raise_error 'Your x1 coordinate must be an integer'
     end
 
     it 'raises an error if x1 out of range' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.horizontal_change(4, 1, 2, "B")}.to raise_error 'Your x1 coordinate is out of range'
+      expect {@bmap.horizontal_change(4, 1, 2, "B")}.to raise_error 'Your x1 coordinate is out of range'
     end
 
     it 'raises an error if x2 not integer' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.horizontal_change(2, "p", 1, "B")}.to raise_error 'Your x2 coordinate must be an integer'
+      expect {@bmap.horizontal_change(2, "p", 1, "B")}.to raise_error 'Your x2 coordinate must be an integer'
     end
 
     it 'raises an error if x2 out of range' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.horizontal_change(1, 4, 2, "B")}.to raise_error 'Your x2 coordinate is out of range'
+      expect {@bmap.horizontal_change(1, 4, 2, "B")}.to raise_error 'Your x2 coordinate is out of range'
     end
 
     it 'raises an error if y not integer' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.horizontal_change(1, 2, "p", "B")}.to raise_error 'Your y coordinate must be an integer'
+      expect {@bmap.horizontal_change(1, 2, "p", "B")}.to raise_error 'Your y coordinate must be an integer'
     end
 
     it 'raises an error if y out of range' do
-      bmap = Bitmap.new(3, 2)
-      expect {bmap.horizontal_change(1, 2, 4, "B")}.to raise_error 'Your y coordinate is out of range'
+      expect {@bmap.horizontal_change(1, 2, 4, "B")}.to raise_error 'Your y coordinate is out of range'
     end
 
     it 'colour represented by capital letter' do
-      bmap = Bitmap.new(3, 2)
-      bmap.horizontal_change(1, 3, 1,"b")
-      expect(bmap.bitmap).to eq([["B", "B", "B"], ["O", "O", "O"]])
+      @bmap.horizontal_change(1, 3, 1,"b")
+      expect(@bmap.bitmap).to eq([["B", "B", "B"], ["O", "O", "O"]])
     end
 
   end
